6a2256b0bd3ff5b576ea54c7e50ab62c936f2864
Abraham Alvarado <abrahamalvarado@devarana.mx>
2022-12-13 12:21:00 -0600

    Home

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,10 @@ import { Form } from '../components/Form'
 import { BannerRedes } from '../components/layout/BannerRedes'
 import { Seo } from '../components/layout/Seo'
 
+import fachada from '../public/assets/images/home/FachadaDEVARANA.webp'
+import credenza from '../public/assets/images/home/Credenza.webp'
+import fotoSalon from '../public/assets/images/home/FotoSalonIngles.webp'
+
 export default function Home() {
   return (
     <>
@@ -14,15 +18,21 @@ export default function Home() {
 
         <div id="initialBanner">
             <div className='h-screen w-full relative object-contain'>
-                <Image src="/assets/images/home/FachadaDEVARANA.webp" className='object-cover w-full' fill alt='Devarana Home' />
+                <Image src={fachada} className='object-cover w-full' fill alt='Devarana Home' />
             </div>
         </div>
         <div className='grid grid-cols-2'>
-            <div className='col-span-1'>
-                <Image /> 
+            <div className='col-span-1 relative pt-1 pr-0.5'>
+                <Image src={credenza} className='object-cover w-full' height={600} width={1000} placeholder="blur" alt="Credenza"/> 
+                <div className='absolute bg-devarana-midnight bg-opacity-50 w-full top-1 left-0 bottom-0 right-1'>
+
+                </div>
             </div>
-            <div className='col-span-1'>
-                <Image />
+            <div className='col-span-1 relative pt-1 pl-0.5'>
+                <Image src={fotoSalon} className='object-cover w-full' height={600} width={1000} placeholder="blur" alt="Salon Ingles"/>
+                <div className='absolute bg-devarana-midnight bg-opacity-50 w-full top-1 left-1 bottom-0 right-0'>
+
+                </div>
             </div>
         </div>
 
